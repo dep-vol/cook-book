f50b55fa6012f2f7a72dcb44ef97982745ba8427
Alexandr Solodovnikov <a.s.solodovnikov@foxford.ru>
2026-05-28 15:51:01 +0300

    feat: wire up CheerioScraper in DI container
    Register CheerioScraper with UrlScraperToken in the Inversify container.
    Binding is placed before ImportJobServiceToken as it is a dependency.

--- src/container.ts
+++ src/container.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { Container } from 'inversify'
 import { RecipeRepositoryToken, RecipeServiceToken } from '@/tokens/recipe.tokens'
 import { ImportJobRepositoryToken, ImportJobServiceToken, RecipeParserToken } from '@/tokens/import-job.tokens'
+import { UrlScraperToken } from '@/tokens/url-scraper.tokens'
 import { RecipeRepository } from '@/modules/recipes/repositories/recipe.repository'
 import { RecipeService } from '@/modules/recipes/services/recipe.service'
 import { ImportJobRepository } from '@/modules/import-jobs/repositories/import-job.repository'
@@ -9,6 +10,7 @@ import { ImportJobService } from '@/modules/import-jobs/services/import-job.serv
 import { RecipeParser } from '@/modules/import-jobs/services/recipe-parser.service'
 import { LLMService } from '@/modules/import-jobs/services/llm.service'
 import { LLMServiceToken } from '@/tokens/import-job.tokens'
+import { CheerioScraper } from '@/modules/url-scraper/cheerio.scraper'
 
 export const container = new Container()
 
@@ -17,5 +19,6 @@ container.bind(RecipeServiceToken).to(RecipeService).inSingletonScope()
 container.bind(ImportJobRepositoryToken).to(ImportJobRepository).inSingletonScope()
 container.bind(LLMServiceToken).to(LLMService).inSingletonScope()
 container.bind(RecipeParserToken).to(RecipeParser).inSingletonScope()
+container.bind(UrlScraperToken).to(CheerioScraper).inSingletonScope()
 container.bind(ImportJobServiceToken).to(ImportJobService).inSingletonScope()
 
