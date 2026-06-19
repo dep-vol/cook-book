e241944649b4680dcda583fd9e1aafdd200d337f
Солодовников Александр <asolodovnikov@nota.tech>
2026-06-19 13:13:22 +0300

    fix(bot): remove unused RecipeBotToken

--- src/modules/bot/bot.tokens.ts
+++ src/modules/bot/bot.tokens.ts
@@ -1,10 +1,9 @@
 // src/modules/bot/bot.tokens.ts
 import type { ServiceIdentifier } from 'inversify'
-import type { IDraftHandler } from './handlers/draft.handler.interface'
-import type { IImportHandler } from './handlers/import.handler.interface'
-import type { ICallbackHandler } from './handlers/callback.handler.interface'
+import type { IDraftHandler }     from './handlers/draft.handler.interface'
+import type { IImportHandler }    from './handlers/import.handler.interface'
+import type { ICallbackHandler }  from './handlers/callback.handler.interface'
 
-export const RecipeBotToken:       ServiceIdentifier<unknown>          = Symbol.for('RecipeBot')
 export const DraftHandlerToken:    ServiceIdentifier<IDraftHandler>    = Symbol.for('DraftHandler')
 export const ImportHandlerToken:   ServiceIdentifier<IImportHandler>   = Symbol.for('ImportHandler')
 export const CallbackHandlerToken: ServiceIdentifier<ICallbackHandler> = Symbol.for('CallbackHandler')
